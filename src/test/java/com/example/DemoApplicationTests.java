26f7e30afabb20907e38eff5a708f49be5bd5f6a
Kyle Wilichowski <kdwilich@uark.edu>
2019-02-18 12:34:49 -0600

    First commit week 1.

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//first week commit done off this exmaple
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
