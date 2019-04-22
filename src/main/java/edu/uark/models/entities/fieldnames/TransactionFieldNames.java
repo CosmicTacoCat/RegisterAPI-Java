03fc98c0349902c1958403c331cac88e070e8f26
James Hand <jehand@uark.edu>
2019-04-22 12:01:56 -0500

    Trans field names changed

--- src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
@@ -4,9 +4,6 @@ import edu.uark.dataaccess.entities.BaseFieldNames;
 
 public class TransactionFieldNames extends BaseFieldNames {
 	public static final String TRANS_ID = "trans_id";
-	public static final String PRODUCT_ID = "product_id";
-	public static final String QUANTITY = "quantity";
-	public static final String SUB_TOTAL = "sub_total";
 	public static final String TOTAL = "total";
 	public static final String ITEMS = "items";
 	public static final String RETURNTYPE = "returntype";
