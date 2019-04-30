a8f9a6a54f4d10cbf14cb3a529ed4a1603584a24
James Hand <jehand@uark.edu>
2019-04-30 13:31:39 -0500

    Transaction field name change

--- src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
@@ -6,7 +6,7 @@ public class TransactionFieldNames extends BaseFieldNames {
 	public static final String TRANS_ID = "trans_id";
 	public static final String TOTAL = "total";
 	public static final String ITEMS = "items";
-	public static final String RETURNTYPE = "returntype";
+	public static final String RETURNTYPE = "return";
 	public static final String RETURN_ID = "return_id";
 	public static final String EMPLOYEE_ID = "employee_id";
 }
